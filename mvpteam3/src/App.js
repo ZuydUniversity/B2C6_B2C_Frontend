d8931dad173790bcd6f32a610f4b4558ecb2ed89
Jeroen Siebers <2204967siebers@zuyd.nl>
2024-05-15 11:56:18 +0200

    eigen backend connected met zoekfunctie

--- mvpteam3/src/App.js
+++ mvpteam3/src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [drinkName, setDrinkName] = useState(null);
+  const [Name, setName] = useState(null);
 
   useEffect(() => {
     if (!searchTerm) {
-      setDrinkName(null); // Reset drinkName if searchTerm is empty
+      setName(null); // Reset drinkName if searchTerm is empty
       return;
     }
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    fetch(`http://pythonbackend.eueahdc3epg9b3a6.westeurope.azurecontainer.io/items/${searchTerm}`)
       .then(response => response.json())
       .then(data => {
-        if (data.drinks && data.drinks.length > 0) {
-          setDrinkName(data.drinks[0].strDrink);
-        } else {
-          setDrinkName('Drink not found');
-        }
+
+          setName(data.description);
+
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -37,13 +35,13 @@ function App() {
       <div class="test">
         <input
           type="text"
-          placeholder="Enter drink name"
+          placeholder="Enter name"
           value={searchTerm}
           onChange={handleInputChange}
         />
         </div>
         <div class="test">
-        <p>Drink Name: {drinkName}</p>
+        <p>Name: {Name}</p>
         </div>
         <img class="Image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.udacity.com%2Fblog%2Fwp-content%2Fuploads%2F2020%2F11%2FHello-World_Blog-scaled.jpeg&f=1&nofb=1&ipt=b66752f2f6a1495594881ad2b96d3838c273feded852265edb11812227253a6b&ipo=images" alt="HELLO WORLD"></img>
       </div>
