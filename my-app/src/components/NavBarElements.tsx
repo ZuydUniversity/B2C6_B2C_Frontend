f3595c7b2386297d9e10f100b7966547cfad5f9d
David Beljaars <davidbel.zakelijk@gmail.com>
2024-05-25 17:03:26 +0200

    root link opgeschoond
    Overbodig component weggehaald/eruit gecodeerd zodat de code wat overzichtelijker is

--- my-app/src/components/NavBarElements.tsx
+++ my-app/src/components/NavBarElements.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
-import Rootlink from './rootlink';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="rootlink">
-          <Rootlink />
+          <Link to="/" className="navbar-link">Home</Link>
         </li>
         <li className="navbar-item">
           <Link to="/Patientpage" className="navbar-link">Patientpage</Link>
--- my-app/src/pages/patientpage.tsx
+++ my-app/src/pages/patientpage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/patientpage.css';
 
 const patientpage: React.FC = () => {
   return (
