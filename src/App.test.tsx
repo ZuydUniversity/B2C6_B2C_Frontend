45578c202fe79f9e855ee6435c950b6f7f22b2a8
nurulgulpen <nurulgulpen@gmail.com>
2024-05-28 20:21:58 +0200

    Code coverage frontend
    Er is een code coverage toegevoegd middels Jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('link', { name: /Home/i });
   expect(linkElement).toBeInTheDocument();
 });
