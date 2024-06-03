0563e461a79c0822506b609125431dbdc42fa0c2
Tristan Franssen <tristanfranssen@gmail.com>
2024-06-03 12:59:12 +0200

    Fixed prettier errors

--- src/components/NavBarElements.tsx
+++ src/components/NavBarElements.tsx
@@ -7,13 +7,19 @@ const NavBar: React.FC = () => {
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="rootlink">
-          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
         </li>
         <li className="navbar-item">
-          <Link to="/Patientpage" className="navbar-link">Patientpage</Link>
+          <Link to="/Patientpage" className="navbar-link">
+            Patientpage
+          </Link>
         </li>
         <li className="navbar-item">
-          <Link to="/Docterpage" className="navbar-link">Doctorpage</Link>
+          <Link to="/Docterpage" className="navbar-link">
+            Doctorpage
+          </Link>
         </li>
       </ul>
     </nav>
