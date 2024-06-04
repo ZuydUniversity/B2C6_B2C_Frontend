1c3953879b3925025d87d75a8cbc63c0689d543f
Tristan Franssen <tristanfranssen@gmail.com>
2024-06-04 11:30:35 +0200

    Made some prettier errors for testing

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>,);
 
 reportWebVitals();
