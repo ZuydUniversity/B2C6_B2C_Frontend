2cd35150dd6cb64a3b98d29cc70311d5026e958d
Jeroen Siebers <2204967siebers@zuyd.nl>
2024-06-04 18:39:27 +0200

    Dashboard navigatie werkt

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import "./styles/dashboardcss.css";
 
 const Dashboard: React.FC = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard page!</p>
-        </div>
+      <div className="about-us-page">
+        <h1>Dashboard</h1>
+        <p>
+          Dashboard van het artsenportaal.
+        </p>
+      </div>
     );
-};
+  };
 
 export default Dashboard;
