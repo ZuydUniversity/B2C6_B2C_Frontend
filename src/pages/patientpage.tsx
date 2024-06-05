8e8c31fbf7526ddd10099503ee5861a55eba6a65
David Beljaars <davidbel.zakelijk@gmail.com>
2024-06-05 15:48:18 +0200

    completed patientlist component
    keep in mind that it is hardcoded since there are no records yet. Will work on adding the navbar once it is also a component and the calenders which should also be components.

--- src/pages/patientpage.tsx
+++ src/pages/patientpage.tsx
@@ -1,15 +1,12 @@
-// src/pages/PatientPage.tsx
 import React from "react";
 import "./styles/patientcss.css";
+import PatientList from "../components/patientlist";
 
 const PatientPage: React.FC = () => {
   return (
     <div className="patient-page">
-      <h1>Patient Page</h1>
-      <p>
-        Welkom op de patienten pagina, hier vind je alle informatie beschikbaar
-        over onze patienten.
-      </p>
+      <h1>Hallo, Dr. Johannes Doe</h1>
+      <PatientList />
     </div>
   );
 };
