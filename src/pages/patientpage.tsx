60e98d792557c84527017f692c3de9377eea05d3
David Beljaars <davidbel.zakelijk@gmail.com>
2024-06-07 13:17:14 +0200

    hotfix own branch

--- src/pages/patientpage.tsx
+++ src/pages/patientpage.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./styles/patientcss.css";
 import PatientList from "../components/patientlist";
-import { Patient } from '../types/types';
-
-const patients: Patient[] = [
-  { id: 1, name: 'John Doe', birthdate: '11/02/2008', diagnosis: 'JDM, monocycles', appointments: '2' },
-  { id: 2, name: 'Jane Smith', birthdate: '11/02/2008', diagnosis: 'Kanker', appointments: '2' },
-  { id: 3, name: 'Alice Johnson', birthdate: '11/02/2008', diagnosis: 'Chlamydia', appointments: '2' },
-];
 
 const PatientPage: React.FC = () => {
   return (
     <div className="patient-page">
       <h1>Patiëntenoverzicht</h1>
-      <PatientList patients={patients} />
+      <PatientList />
     </div>
   );
 };
