f91e941a31813da7bdaee569b76ecd0d7cf1594a
David Beljaars <davidbel.zakelijk@gmail.com>
2024-06-07 13:57:39 +0200

    deleted nurul accidental merge/changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import AboutUsPage from "./pages/aboutuspage";
 import PatientPage from "./pages/patientpage";
 import ArtsPage from "./pages/artspage";
 import LandingPage from "./pages/landingpage";
-import PatientOverview from "./pages/patientoverview";
 
 const App: React.FC = () => {
   return (
@@ -13,7 +12,6 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/patients" element={<PatientPage />} />
-        <Route path="/patient/:id" element={<PatientOverview />} />
         <Route path="/artsen" element={<ArtsPage />} />
       </Routes>
     </Router>
