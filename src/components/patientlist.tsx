0b13a2813a4b1ca86d4e012a632230d320f3e2a5
David Beljaars <davidbel.zakelijk@gmail.com>
2024-06-07 11:47:54 +0200

    new component patientlist
    made scrollable patientlist this time.

--- src/components/patientlist.tsx
+++ src/components/patientlist.tsx
@@ -2,14 +2,40 @@ import './componentstyles/patientlist.css';
 
 const PatientList = () => {
   const patients = [
-    { id: 1, name: 'John Doe', age: 30, diagnosis: 'JDM', medication: 'x medicijn', appointments: '3', image: 'https://via.placeholder.com/150', nextAppointment: "02-01-2024"},
-    { id: 2, name: 'John Doe', age: 30, diagnosis: 'JDM', medication: 'x medicijn', appointments: '3', image: 'https://via.placeholder.com/150', nextAppointment: "02-01-2024"},
-    { id: 3, name: 'John Doe', age: 30, diagnosis: 'JDM', medication: 'x medicijn', appointments: '3', image: 'https://via.placeholder.com/150', nextAppointment: "02-01-2024"},
+    { id: 1, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 2, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 3, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 4, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '4'},
+    { id: 1, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 2, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 3, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '3'},
+    { id: 4, name: 'John Doe', geboortedatum: '01-01-1990', diagnosis: 'JDM', medication: 'x medicijn', appointments: '4'},
   ];
 
   return (
     <div className="patientlist-container">
-    
+      <h4 className="patient-title">Alle patienten</h4>
+      <div className="patientlist-header">
+        <div className="patient-label-container">
+          <div>naam</div>
+          <div>geboorte datum</div>
+          <div>diagnose</div>
+          <div>afspraken</div>
+        </div>
+      </div>
+      {patients.map(patient => (
+        <div key={patient.id} className="patient-row-wrapper">
+          <div className="patient-row">
+            <div className="patient-value-container">
+              <div className="patient-value">{patient.name}</div>
+              <div className="patient-value">{patient.geboortedatum}</div>
+              <div className="patient-value">{patient.diagnosis}</div>
+              <div className="patient-value">{patient.appointments}</div>
+            </div>
+          </div>
+          <div className="extra-card">...</div>
+        </div>
+      ))}
     </div>
   );
 };
