181070c12facb187d487ddda6c45848ecf780126
nurulgulpen <nurulgulpen@gmail.com>
2024-06-08 12:41:27 +0200

    Revert "Merge branch 'main' into 70-create-basisstructuur-overzichpatient-pagefrontend"
    This reverts commit 9159c0c86922730335dd103170a4a3984ad99ac8, reversing
    changes made to cfcaacb45cfe600865a1e3d87ad560aca38bb8b9.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AboutUsPage from "./pages/aboutuspage";
 import PatientPage from "./pages/patientpage";
 import ArtsPage from "./pages/artspage";
 import LandingPage from "./pages/landingpage";
-import DashboardPage from "./pages/dashboardpage";
+import PatientOverview from "./pages/patientoverview";
 
 const App: React.FC = () => {
   return (
@@ -13,8 +13,8 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/patients" element={<PatientPage />} />
+        <Route path="/patient/:id" element={<PatientOverview />} />
         <Route path="/artsen" element={<ArtsPage />} />
-        <Route path="/dashboardpage" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+// src/types/types.ts
+export interface Patient {
+  id: number;
+  name: string;
+  birthdate: string;
+  diagnosis: string;
+  appointments: string;
+}
+
+  
+  export interface PatientDetailsProps {
+    patients: Patient[];
+  }
+  
+  export interface ContactPerson {
+    name: string;
+    phone: string;
+    email: string;
+  }
+  
