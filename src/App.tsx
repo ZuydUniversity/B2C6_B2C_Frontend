dfd6c25b0cb2606125223dbb682090b32384d62b
nurulgulpen <nurulgulpen@gmail.com>
2024-06-08 13:07:40 +0200

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import PatientPage from "./pages/patientpage";
 import ArtsPage from "./pages/artspage";
 import LandingPage from "./pages/landingpage";
 import DashboardPage from "./pages/dashboardpage";
+import PatientOverview from "./pages/patientoverview";
+
 
 const App: React.FC = () => {
   return (
@@ -13,6 +15,7 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/patients" element={<PatientPage />} />
+        <Route path="/patient/:id" element={<PatientOverview />} />
         <Route path="/artsen" element={<ArtsPage />} />
         <Route path="/dashboardpage" element={<DashboardPage />} />
       </Routes>
