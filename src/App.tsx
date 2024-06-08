d2e071d83b6a5705968440ecbf6a3397eb135896
nurulgulpen <nurulgulpen@gmail.com>
2024-06-08 13:00:48 +0200

    Reapply "Merge branch 'main' into 70-create-basisstructuur-overzichpatient-pagefrontend"
    This reverts commit 181070c12facb187d487ddda6c45848ecf780126.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AboutUsPage from "./pages/aboutuspage";
 import PatientPage from "./pages/patientpage";
 import ArtsPage from "./pages/artspage";
 import LandingPage from "./pages/landingpage";
-import PatientOverview from "./pages/patientoverview";
+import DashboardPage from "./pages/dashboardpage";
 
 const App: React.FC = () => {
   return (
@@ -13,8 +13,8 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/patients" element={<PatientPage />} />
-        <Route path="/patient/:id" element={<PatientOverview />} />
         <Route path="/artsen" element={<ArtsPage />} />
+        <Route path="/dashboardpage" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
--- src/components/patientfunctionaldetails.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './patientdetails.css';
-
-const PatientFunctionalDetails = () => {
-  // Your component logic here
-};
-
-export default PatientFunctionalDetails;
