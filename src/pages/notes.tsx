38dc16dc28f2863a1546ac1ca257f2b8af81e1a2
Levi Funk <Levifunk01@gmail.com>
2024-06-10 17:28:32 +0200

    Added Notes page to App.tsx routes

---
+++ src/pages/notes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notes: React.FC = () => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <p>These are the notes</p>
+    </div>
+  );
+}
+
+export default Notes;
