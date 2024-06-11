3f0fb62d2cfeadcc2524dca2d6c232a71cf80067
Levi Funk <Levifunk01@gmail.com>
2024-06-11 22:08:37 +0200

    added base divs voor ui

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./styles/Notes.css";
 
 const Notes: React.FC = () => {
   return (
-    <div>
-      <h1>Notes</h1>
-      <p>These are the notes</p>
+    <div className="notes-background">
+      // Title bar in container
+      <div className="notes-title">
+
+      </div>
+
+      // Container for the list of notes
+      <div className="notes-container">
+
+      </div>
     </div>
   );
 }
