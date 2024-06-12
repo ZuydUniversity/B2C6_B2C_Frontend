5cfd9d0b3bd03b46b38779a1747c3da10251277b
jouryl00 <loojoury@gmail.com>
2024-06-12 23:07:30 +0200

    createpatients compfile aangemaake

---
+++ src/components/createpatient.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./componentstyles/HIERZOAANPASSENLATER.css";
+import { Patient } from "../abstracts/ImportsModels";
+
