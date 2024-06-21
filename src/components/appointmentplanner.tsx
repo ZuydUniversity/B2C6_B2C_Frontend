890b8e8424b21cae7a1d019132b1657e368d8894
Nessen03 <115788592+Nessen03@users.noreply.github.com>
2024-06-21 11:33:58 +0200

    added herhaling title
    Co-Authored-By: Kelvin Wetzels <129733070+KamikazeShot@users.noreply.github.com>

--- src/components/appointmentplanner.tsx
+++ src/components/appointmentplanner.tsx
@@ -22,7 +22,7 @@ const AppointmentPlanner: React.FC = () => {
   return (
     <div className="appointment-planner">
       <h1 className="title-with-underline">Afspraak toevoegen</h1>
-      <h1 className="title-of-element">Titel</h1>
+      <h1 className="title-of-title-bar">Titel</h1>
       <div className="appointment-form">
         <input
           type="text"
@@ -32,7 +32,8 @@ const AppointmentPlanner: React.FC = () => {
           className="title-input"
         />
       </div>
-
+      <h1 className="title-of-repetition">Herhaling</h1>
+      <h1 className="title-of-repetition-optional">(Optioneel)</h1>
     </div>
   );
 };
