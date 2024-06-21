11a0a6ceb5062a27dfe44d2c9729fada02c655ff
DeNaamIsDaaf <DeNaamIsDaaf@users.noreply.github.com>
2024-06-21 11:19:16 +0000

    Apply automatic changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,32 +14,26 @@ import ArtsSessionPage from "./pages/artssessionpage";
 const App: React.FC = () => {
 	const [hideNavbar, setHideNavbar] = useState(false);
 
-  return (
-    <Router>
-      <div className="app-container">
-        {!hideNavbar && <Sidebar />}
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route
-              path="/login"
-              element={<LoginPage setHideNavbar={setHideNavbar} />}
-            />
-            <Route path="/aboutus" element={<AboutUsPage />} />
-            <Route path="/patients" element={<PatientPage />} />
-            <Route path="/patient/:id" element={<PatientOverview />} />
-            <Route path="/artsen" element={<ArtsPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/notes" element={<Notes />} />
-            <Route path="/artsession" element={<ArtsSessionPage />} />
-            <Route
-              path="*"
-              element={<NotFoundPage setHideNavbar={setHideNavbar} />}
-            />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app-container">
+				{!hideNavbar && <Sidebar />}
+				<div className="main-content">
+					<Routes>
+						<Route path="/" element={<Navigate to="/login" replace />} />
+						<Route path="/login" element={<LoginPage setHideNavbar={setHideNavbar} />} />
+						<Route path="/aboutus" element={<AboutUsPage />} />
+						<Route path="/patients" element={<PatientPage />} />
+						<Route path="/patient/:id" element={<PatientOverview />} />
+						<Route path="/artsen" element={<ArtsPage />} />
+						<Route path="/dashboard" element={<DashboardPage />} />
+						<Route path="/notes" element={<Notes />} />
+						<Route path="/artsession" element={<ArtsSessionPage />} />
+						<Route path="*" element={<NotFoundPage setHideNavbar={setHideNavbar} />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 };
 export default App;
