bd0f955233b27259b0f1c8849ec37668ef18591d
Megalomizer <112645496+Megalomizer@users.noreply.github.com>
2024-06-23 23:10:54 +0200

    Began development for component, basic rendering and styling

---
+++ src/components/dashboardsettings.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./componentstyles/dashboardsettings.css";
+
+const DashboardSettings: React.FC = () => {
+  return (
+    <div className="settings-containers-background">
+      <div className="settings-container">
+        <div className="settings-item-container">
+          <div className="settings-item-element">
+            SETTING SHIT
+          </div>
+          <div className="settings-item-switch"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
