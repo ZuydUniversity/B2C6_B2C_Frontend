bb3f9cf62128c702d54369e306a79cbffb85ce94
Tristan Franssen <tristanfranssen@gmail.com>
2024-06-26 13:31:53 +0200

    Updated login page to use right url and personel number

---
+++ src/abstracts/Constances.ts
@@ -0,0 +1,5 @@
+export const apiUrl = (process.env.HOSTNAME === 'docker' || process.env.DOCKER === 'true') ? "https://backend.myolink.info.gf/api/" : "localhost:8000/api/";
+
+export interface Dictionary<T> {
+	[Key: string]: T;
+}
