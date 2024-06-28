81b118d1fa1cb5f3a49d455d0bdd7f3a24552fd8
bilalb41 <113174721+bilalb41@users.noreply.github.com>
2024-06-28 14:04:38 +0200

    Add test for ActiveArtsSession

---
+++ src/tests/Pagetests/activeartssessionpage.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ActiveArtsSession from "../../pages/activeartssessionpage";
+
+describe("ActiveArtsSession", () => {
+    it("renders without errors", () => {
+        render(<ActiveArtsSession />);
+    });
+});
