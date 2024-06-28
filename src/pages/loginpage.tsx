d8a4daeb660aea23503b7ee126f0c7a099262116
SnailCape <SnailCape@users.noreply.github.com>
2024-06-28 12:12:39 +0000

    Apply automatic changes

--- src/pages/loginpage.tsx
+++ src/pages/loginpage.tsx
@@ -77,7 +77,9 @@ const LoginPage: React.FC<Props> = ({ setHideNavbar }) => {
 							<input type="password" id="password" name="password" placeholder="Wachtwoord" onChange={(e) => setPassword(e.target.value)} />
 						</div>
 						<div className="form-group action-group">
-							<button className="loginpage-button" onClick={handleLogin}>Log in</button>
+							<button className="loginpage-button" onClick={handleLogin}>
+								Log in
+							</button>
 							<a href="#" onClick={openPopup}>
 								Wachtwoord vergeten?
 							</a>
--- src/tests/Pagetests/activeartssessionpage.test.tsx
+++ src/tests/Pagetests/activeartssessionpage.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import ActiveArtsSession from "../../pages/activeartssessionpage";
 
 describe("ActiveArtsSession", () => {
-    it("renders without errors", () => {
-        render(<ActiveArtsSession />);
-    });
+	it("renders without errors", () => {
+		render(<ActiveArtsSession />);
+	});
 });
