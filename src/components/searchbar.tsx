cc41b02e8ae47153934ea6225e9b7c69b6492b3b
SnailCape <koopacapey@gmail.comn>
2024-07-01 14:40:15 +0200

    Made a searchbar component for the notes. Ofcourse this can be imported wherever you may want in the app

---
+++ src/components/searchbar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import "./componentstyles/searchbar.css";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+    onSearch(newQuery);
+  };
+
+  return (
+    <div className="searchbar-outer">
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
