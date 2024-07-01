4ab11fdee3913d81a5b59bc759e3f5f5cf1273aa
KamikazeShot <129733070+KamikazeShot@users.noreply.github.com>
2024-07-01 14:54:57 +0200

    Updates to calenderpage.test.tsx

--- src/tests/Pagetests/calenderpage.test.tsx
+++ src/tests/Pagetests/calenderpage.test.tsx
@@ -26,7 +26,7 @@ const getStartOfWeek = (date: Date): Date => {
 
 const getDateOfISOWeek = (week: number, year: number): Date => {
 	if (week < 1 || week > 52) {
-		throw new Error("Invalid week");
+		throw new Error("Invalid week number");
 	}
 	if (year < 1) {
 		throw new Error("Invalid year");
@@ -66,32 +66,28 @@ describe("CalenderPage", () => {
 		expect(startDateElement.length).toBeGreaterThan(0);
 	});
 
-	it("navigates to the previous week", async () => {
+	it("navigates to the previous week", () => {
 		render(<CalenderPage />);
 		const prevButton = screen.getByRole("button", { name: /</ });
 		fireEvent.click(prevButton);
-		await waitFor(() => {
-			const previousWeekDate = getStartOfWeek(new Date());
-			previousWeekDate.setDate(previousWeekDate.getDate() - 7);
-			const monthNames = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
-			const formattedPrevWeekDate = `${String(previousWeekDate.getDate()).padStart(2, "0")} ${monthNames[previousWeekDate.getMonth()]}`;
-			const prevWeekDateElement = screen.getAllByText((content, element) => (element && element.tagName.toLowerCase() === "th" && content.includes(formattedPrevWeekDate) ? true : false));
-			expect(prevWeekDateElement.length).toBeGreaterThan(0);
-		});
+		const previousWeekDate = getStartOfWeek(new Date());
+		previousWeekDate.setDate(previousWeekDate.getDate() - 7);
+		const monthNames = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
+		const formattedPrevWeekDate = `${String(previousWeekDate.getDate()).padStart(2, "0")} ${monthNames[previousWeekDate.getMonth()]}`;
+		const prevWeekDateElement = screen.getAllByText((content, element) => (element && element.tagName.toLowerCase() === "th" && content.includes(formattedPrevWeekDate) ? true : false));
+		expect(prevWeekDateElement.length).toBeGreaterThan(0);
 	});
 
-	it("navigates to the next week", async () => {
+	it("navigates to the next week", () => {
 		render(<CalenderPage />);
 		const nextButton = screen.getByRole("button", { name: />/ });
 		fireEvent.click(nextButton);
-		await waitFor(() => {
-			const nextWeekDate = getStartOfWeek(new Date());
-			nextWeekDate.setDate(nextWeekDate.getDate() + 7);
-			const monthNames = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
-			const formattedNextWeekDate = `${String(nextWeekDate.getDate()).padStart(2, "0")} ${monthNames[nextWeekDate.getMonth()]}`;
-			const nextWeekDateElement = screen.getAllByText((content, element) => (element && element.tagName.toLowerCase() === "th" && content.includes(formattedNextWeekDate) ? true : false));
-			expect(nextWeekDateElement.length).toBeGreaterThan(0);
-		});
+		const nextWeekDate = getStartOfWeek(new Date());
+		nextWeekDate.setDate(nextWeekDate.getDate() + 7);
+		const monthNames = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
+		const formattedNextWeekDate = `${String(nextWeekDate.getDate()).padStart(2, "0")} ${monthNames[nextWeekDate.getMonth()]}`;
+		const nextWeekDateElement = screen.getAllByText((content, element) => (element && element.tagName.toLowerCase() === "th" && content.includes(formattedNextWeekDate) ? true : false));
+		expect(nextWeekDateElement.length).toBeGreaterThan(0);
 	});
 
 	it("changes week number from dropdown", async () => {
@@ -105,30 +101,26 @@ describe("CalenderPage", () => {
 		});
 	});
 
-	it("calls handlePrevWeek on previous week button click", async () => {
+	it("calls handlePrevWeek on previous week button click", () => {
 		render(<CalenderPage />);
 		const prevButton = screen.getByRole("button", { name: /</ });
 		fireEvent.click(prevButton);
-		await waitFor(() => {
-			const newDate = new Date();
-			newDate.setDate(newDate.getDate() - 7);
-			const currentWeekNumber = getWeekNumber(getStartOfWeek(newDate));
-			const weekNumberOption = screen.getByText(`Week ${currentWeekNumber}`);
-			expect(weekNumberOption).toBeInTheDocument();
-		});
+		const newDate = new Date();
+		newDate.setDate(newDate.getDate() - 7);
+		const currentWeekNumber = getWeekNumber(getStartOfWeek(newDate));
+		const weekNumberOption = screen.getByText(`Week ${currentWeekNumber}`);
+		expect(weekNumberOption).toBeInTheDocument();
 	});
 
-	it("calls handleNextWeek on next week button click", async () => {
+	it("calls handleNextWeek on next week button click", () => {
 		render(<CalenderPage />);
 		const nextButton = screen.getByRole("button", { name: />/ });
 		fireEvent.click(nextButton);
-		await waitFor(() => {
-			const newDate = new Date();
-			newDate.setDate(newDate.getDate() + 7);
-			const currentWeekNumber = getWeekNumber(getStartOfWeek(newDate));
-			const weekNumberOption = screen.getByText(`Week ${currentWeekNumber}`);
-			expect(weekNumberOption).toBeInTheDocument();
-		});
+		const newDate = new Date();
+		newDate.setDate(newDate.getDate() + 7);
+		const currentWeekNumber = getWeekNumber(getStartOfWeek(newDate));
+		const weekNumberOption = screen.getByText(`Week ${currentWeekNumber}`);
+		expect(weekNumberOption).toBeInTheDocument();
 	});
 
 	it("renders all days of the week correctly", async () => {
@@ -148,26 +140,6 @@ describe("CalenderPage", () => {
 		times.forEach((time) => {
 			expect(screen.getByText(time)).toBeInTheDocument();
 		});
-	});
-
-	it("handles edge cases for handlePrevWeek and handleNextWeek", async () => {
-		render(<CalenderPage />);
-		const prevButton = screen.getByRole("button", { name: /</ });
-		const nextButton = screen.getByRole("button", { name: />/ });
-
-		// Navigate to the first week of the year
-		for (let i = 0; i < 52; i++) {
-			fireEvent.click(prevButton);
-			await waitFor(() => screen.getByText(/Week 1/i));
-		}
-		expect(screen.getByText(/Week 1/i)).toBeInTheDocument();
-
-		// Navigate to the last week of the year
-		for (let i = 0; i < 52; i++) {
-			fireEvent.click(nextButton);
-			await waitFor(() => screen.getByText(/Week 52/i));
-		}
-		expect(screen.getByText(/Week 52/i)).toBeInTheDocument();
 	});
 
 	it("handles edge cases for handleWeekChange", async () => {
@@ -214,7 +186,7 @@ describe("Negative tests for helper functions", () => {
 	});
 
 	it("handles invalid week and year in getDateOfISOWeek", () => {
-		expect(() => getDateOfISOWeek(53, 2022)).toThrow("Invalid week");
+		expect(() => getDateOfISOWeek(53, 2022)).toThrow("Invalid week number");
 		expect(() => getDateOfISOWeek(1, -1)).toThrow("Invalid year");
 	});
 });
@@ -224,7 +196,7 @@ describe("Helper functions", () => {
 	it("getWeekNumber works correctly", () => {
 		expect(getWeekNumber(new Date("2022-01-01"))).toBe(1);
 		expect(getWeekNumber(new Date("2022-01-02"))).toBe(2);
-		expect(getWeekNumber(new Date("2022-12-31"))).toBe(54);
+		expect(getWeekNumber(new Date("2022-12-31"))).toBe(53);
 	});
 
 	it("getStartOfWeek works correctly", () => {
@@ -233,7 +205,7 @@ describe("Helper functions", () => {
 	});
 
 	it("getDateOfISOWeek works correctly", () => {
-		expect(getDateOfISOWeek(1, 2022).toISOString().split("T")[0]).toBe("2022-01-02");
+		expect(getDateOfISOWeek(1, 2022).toISOString().split("T")[0]).toBe("2022-01-03");
 		expect(getDateOfISOWeek(52, 2022).toISOString().split("T")[0]).toBe("2022-12-25");
 	});
 });
