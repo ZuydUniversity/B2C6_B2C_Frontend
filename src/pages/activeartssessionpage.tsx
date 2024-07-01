4e12a5dbd2260c865b997181a7e4642775975bbd
OFFTijn <OFFTijn@users.noreply.github.com>
2024-07-01 10:39:52 +0000

    Apply automatic changes

--- src/pages/activeartssessionpage.tsx
+++ src/pages/activeartssessionpage.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import search_icon from "./images/search_icon.png"; 
+import search_icon from "./images/search_icon.png";
 import "./styles/activeartssession.css";
 import SessionScanner from "../components/sessionscanner";
 
