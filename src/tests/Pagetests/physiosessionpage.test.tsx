274b33921c75cf1082525664fcdb67693a2f56cb
Jeroen Siebers <2204967siebers@zuyd.nl>
2024-07-04 12:13:26 +0200

    Upped physiosessionpage coverage

--- src/tests/Pagetests/physiosessionpage.test.tsx
+++ src/tests/Pagetests/physiosessionpage.test.tsx
@@ -1,8 +1,50 @@
+import { PatientSelect } from "../../pages/artssessionpage";
+import AddPatientSelection from "../../pages/physiosessionpage";
 import PhysioSessionPage from "../../pages/physiosessionpage";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 describe("PhysioSessionPage test", () => {
 	it("Should rendere page without failing", () => {
 		render(<PhysioSessionPage />);
 	});
+	it("Should toggle patient list visibility on button click", () => {
+		const { getByText, queryByText } = render(<AddPatientSelection />);
+		const selectPatientButton = getByText("Selecteer patiënt");
+		expect(queryByText("Naam:")).not.toBeInTheDocument(); 
+	
+		fireEvent.click(selectPatientButton); 
+		expect(queryByText("Naam:")).not.toBeInTheDocument(); 
+	});
+
+	it("Should hide patient list when clicking outside", () => {
+		const { getByText, queryByRole, getByRole } = render(<PatientSelect onSelect={() => {}} />);
+	
+		expect(getByRole("list")).toBeInTheDocument();
+	
+		const outsideClickEvent = new MouseEvent("mousedown", {
+			bubbles: true,
+			cancelable: true,
+			view: window,
+		});
+		document.dispatchEvent(outsideClickEvent);
+	
+		expect(queryByRole("list")).not.toBeInTheDocument();
+	});
+
+	it("Should correctly display patient information when selected", () => {
+        const { getByText, getAllByRole } = render(<AddPatientSelection />);
+        const selectPatientButton = getByText("Selecteer patiënt");
+        fireEvent.click(selectPatientButton); 
+
+        const patientButtons = getAllByRole("button"); 
+        fireEvent.click(patientButtons[0]); 
+    });
+
+	it("Should correctly display all patients in the list", () => {
+        const { getByText, getAllByRole } = render(<AddPatientSelection />);
+        const selectPatientButton = getByText("Selecteer patiënt");
+        fireEvent.click(selectPatientButton); 
+
+        const patientButtons = getAllByRole("button");
+    });
 });
