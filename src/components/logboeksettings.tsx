a7ebc05f7265a2c34264a08a3be6b81d7d3f0bdb
Megalomizer <112645496+Megalomizer@users.noreply.github.com>
2024-07-07 22:39:33 +0200

    Added logboek settings component

--- src/components/logboeksettings.tsx
+++ src/components/logboeksettings.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./componentstyles/dashboardsettings.css";
-import SettingSwitch from "./switchsetting";
+import SettingsElement from "./settingselement";
 
-const DashboardSettings: React.FC = () => {
+const LogboekSettings: React.FC = () => {
 
   return (
     <div className="settings-containers-background">
       <div className="settings-container">
-        <SettingSwitch status={false} />
+      <SettingsElement status={false} title="Compact mode"/>
+      <SettingsElement status={false} title="Dark theme"/>
       </div>
     </div>
   );
 }
 
-export default DashboardSettings;
+export default LogboekSettings;
